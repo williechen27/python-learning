67abc46fbe2cb389ff5c0de1176842aacd55e793
williechen27 <williechen27@gmail.com>
2026-07-08 17:08:03 +0800

    Update q7.py

--- EX1/q7.py
+++ EX1/q7.py
@@ -11,3 +11,19 @@ if password[0].isupper():
         print('密碼不可全為大寫')
 else:
     print('密碼必須以大寫字母開頭')
+
+######## Enhanced version ########
+
+password = input('請輸入密碼：')
+
+if not password:
+    print('密碼不可為空')
+elif not password[0].isupper():
+    print('密碼必須以大寫字母開頭')
+elif password.isupper():
+    print('密碼不可全為大寫')
+elif not (7 <= len(password) <= 12):
+    print('密碼長度不符合規定')
+else:
+    masked = password[:2] + '*' * (len(password) - 3) + password[-1]
+    print(f'{masked}')
