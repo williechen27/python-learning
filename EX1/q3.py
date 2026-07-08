466a69394bd2d405b25fd75dbd2440fa19caaa3c
williechen27 <williechen27@gmail.com>
2026-07-08 17:03:06 +0800

    Create q3.py

---
+++ EX1/q3.py
@@ -0,0 +1,9 @@
+food1 = 'roast beef'
+food2 = 'ham'
+body = 'head'
+animal = 'clam'
+
+print('My kitty cat likes %s,' % food1)
+print('My kitty cat likes %s,' % food2)
+print('My kitty cat fell on his %s' % body)
+print("And now thinks he's a %s." % animal)
